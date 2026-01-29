c09425dba92d25b8d4bcfda7f8e36466e889cbdd
Tatenda Ganyani <tatendaganyani5@gmail.com>
2026-01-30 01:48:48 +0300

    changed next.config

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    // Needed for local object URLs (preview only)
-    remotePatterns: [],
-    domains: ["res.cloudinary.com"],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        
+      },
+    ],
   },
 };
 
